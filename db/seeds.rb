43d62ccab09e73ac39fad2da3ac3fd5cad6ec16d
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-09-06 23:39:25 +0900

    UserInfo初期データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,3 +20,15 @@ User.create(
   password: 'password', 
   nickname: 'user1'
 )
+
+UserInfo.create(
+  user_id: 1,
+  firstname: 'user',
+  lastname: '1',
+  zipcode: '1111111',
+  prefecture: 1,
+  city: 'test2',
+  block: 'test3',
+  building: 'test4',
+  phone: '00000000000',
+)
