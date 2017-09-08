905d06066ad4ae4cd1e2bc82f0e623463e9e0940
nekosuke0728 <hatarakazarumono.kuubekarazu00@gmail.com>
2017-09-08 16:15:47 +0900

    cart_controller,view、修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,17 +2,14 @@ class CartsController < ApplicationController
   before_action :authenticate_user!
 
   add_breadcrumb 'トップページ', :shop_top_path
+  add_breadcrumb '商品一覧', :products_path
+  add_breadcrumb '買い物カゴ' 
 
   def show
     @cart = current_user.cart
-    if @cart.nil?
-       @error = "買い物カゴは空です"
-       render shop_top_path
-    else      
+    unless @cart.nil?
       @cart_product = @cart.cart_products
-      add_breadcrumb '商品一覧', :products_path
-      add_breadcrumb '買い物カゴ'
     end
   end
-  
+
 end
